0c1e6f4b6b2d5584e5930488a018ed2f375f6a66
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2025-08-22 12:53:10 -0300

    [CDG-9][CDG-11] Initial login layout

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,20 @@
+import { Button, Flex, TextField } from "@radix-ui/themes";
+
 const Login = () => {
   return (
-    <div className="login-container">
+    <Flex direction="column" align="center" justify="center" gap="4" m="9">
       <h1>Login Page</h1>
-      {/* Add your login form here */}
-    </div>
+      <TextField.Root placeholder="Enter your username" size="3" />
+      <TextField.Root
+        placeholder="Enter your password"
+        type="password"
+        size="3"
+      />
+      <Button size="3" style={{ width: "200px" }}>
+        Entrar
+      </Button>
+      <Button variant="ghost">Registre-se</Button>
+    </Flex>
   );
 };
 
