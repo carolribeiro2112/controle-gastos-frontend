ab83d0d41d380f5d483fc773e629857d6961ba16
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2025-08-24 12:41:49 -0300

    [CDG-9][CDG-11] Add register page to router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
