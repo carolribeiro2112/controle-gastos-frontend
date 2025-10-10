2ed36d567718f61ac250188729b3b506a9b1e5dd
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2025-10-09 21:17:01 -0300

    [CDG-82][CDG-84] - Add translations for login page

--- src/i18n/languages/en-US.ts
+++ src/i18n/languages/en-US.ts
@@ -6,6 +6,15 @@ const enUS: MessageMap = {
   },
   login: {
     title: "Login to your account",
+    usernamePrompt: "Please enter your username",
+    passwordPrompt: "Please enter your password",
+    loginError: "Login error",
+    invalidCredentials: "Invalid credentials, try again or sign up",
+    userNamePlaceholder: "Enter your username",
+    passwordPlaceholder: "Enter your password",
+    loggingIn: "Logging in...",
+    logIn: "Log in",
+    signUp: "Sign up"
   }
 };
 
--- src/i18n/languages/pt-BR.ts
+++ src/i18n/languages/pt-BR.ts
@@ -6,6 +6,15 @@ const ptBR: MessageMap = {
   },
   login: {
     title: "Entre na sua conta",
+    usernamePrompt: "Por favor, insira seu nome de usuário",
+    passwordPrompt: "Por favor, insira sua senha",
+    loginError: "Erro durante o login",
+    invalidCredentials: "Credenciais inválidas, tente novamente ou cadastre-se",
+    userNamePlaceholder: "Insira o nome de usuário",
+    passwordPlaceholder: "Insira a senha",
+    loggingIn: "Entrando...",
+    logIn: "Entrar",
+    signUp: "Registre-se"
   }
 };
 
